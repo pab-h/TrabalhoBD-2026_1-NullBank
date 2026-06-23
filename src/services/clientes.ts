6b461edc960266610ea716726901a62954d989bb
LairtonPessoa <franciscolairton.contato@gmail.com>
2026-06-23 10:04:09 -0300

    feat: implementação do serviço de busca para o cliente

--- src/services/clientes.ts
+++ src/services/clientes.ts
@@ -3,10 +3,28 @@ import { pool } from "../database";
 
 export class ClientesService {
 
-    index = async (request: FastifyRequest, reply: FastifyReply) => {
-        
-        reply.status(200).send({ exemplo: "Exemplo!" });
+    getContasCliente = async (request: FastifyRequest, reply: FastifyReply) => {
+        const { cpf } = request.params as { cpf: string };
 
+        try {
+            const querry = `
+            SELECT cb.num_conta, cb.tipo_conta, cb.saldo, a.nome_ag AS agencia, f.nome_completo AS gerente
+            FROM conta_bancaria cb
+            JOIN titularidade t ON cb.num_conta = t.fk_num_conta
+            JOIN agencia a ON cb.fk_num_ag = a.num_ag
+            JOIN funcionario f ON cb.fk_matricula_gerente = f.matricula
+            WHERE t.fk_cpf_cliente = ?
+            `;
+            const [rows] = await pool.query(querry, [cpf]);
+            return reply.send(200).send(rows);
+        }        catch (error) {
+            return reply.status(500).send({ error: "Erro ao buscar contas do cliente" });
+        }
     }
 
+    getContasConjuntas = async (request: FastifyRequest, reply: FastifyReply) => {
+        const { cpf } = request.params as { cpf: string };
+
+        try
+    }
 }
