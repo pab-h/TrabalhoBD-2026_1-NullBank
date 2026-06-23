defa6864c5111c7c5117220a5f93149af468d46b
LairtonPessoa <franciscolairton.contato@gmail.com>
2026-06-23 10:07:21 -0300

    feat: implementação do serviço de busca para o cliente pt2

--- src/services/clientes.ts
+++ src/services/clientes.ts
@@ -16,15 +16,80 @@ export class ClientesService {
             WHERE t.fk_cpf_cliente = ?
             `;
             const [rows] = await pool.query(querry, [cpf]);
-            return reply.send(200).send(rows);
-        }        catch (error) {
+            return reply.status(200).send(rows);
+        } catch (error) {
             return reply.status(500).send({ error: "Erro ao buscar contas do cliente" });
         }
-    }
+    };
 
     getContasConjuntas = async (request: FastifyRequest, reply: FastifyReply) => {
         const { cpf } = request.params as { cpf: string };
 
-        try
-    }
+        try {
+            const querry = `
+            SELECT c.nome_completo, c.cpf, t2.fk_num_conta AS num_conta
+            FROM titularidade t1
+            JOIN titularidade t2 ON t1.fk_num_conta = t2.fk_num_conta AND t1.fk_cpf_cliente != t2.fk_cpf_cliente
+            JOIN cliente c ON t2.fk_cpf_cliente = c.cpf
+            WHERE t1.fk_cpf_cliente = ?
+            `;
+            const [rows] = await pool.query(querry, [cpf]);
+            return reply.status(200).send(rows);
+        } catch (error) {
+            return reply.status(500).send({ error: "Erro ao buscar contas conjuntas" });
+        }
+    };
+
+    getContasCorrentesMovimentadas = async (request: FastifyRequest, reply: FastifyReply) => {
+        const { cpf } = request.params as { cpf: string };
+        const { periodo } = request.query as { periodo: "7d" | "30d" | "365d" };
+
+        if (!periodo) {
+            return reply.status(400).send({ error: "Parâmetro 'período' obrigatório" });
+
+        }
+        const dias = parseInt(periodo.replace("d", ""), 10);
+
+        try {
+            const querry = `
+            SELECT cb.num_conta, COUNT(tr.num_transacao) AS total_transacoes
+            FROM conta_bancaria cb
+            JOIN titularidade t ON cb.num_conta = t.fk_num_conta
+            JOIN transacao tr ON cb.num_conta = tr.fk_num_conta
+            WHERE t.fk_cpf_cliente = ? AND cb.tipo_conta = 'conta-corrente' AND tr.data_hora >= DATE_SUB(NOW(), INTERVAL ? DAY)
+            GROUP BY cb.num_conta
+            ORDER BY total_transacoes DESC
+            `;
+            const [rows] = await pool.query(querry, [cpf, dias]);
+            return reply.status(200).send(rows);
+        } catch (error) {
+
+        }
+    };
+
+    getContasMaiorVolume = async (request: FastifyRequest, reply: FastifyReply) => {
+        const { cpf } = request.params as { cpf: string };
+        const { periodo } = request.query as { periodo: "7d" | "30d" | "365d" };
+
+        if (!periodo) {
+            return reply.status(400).send({ error: "Parâmetro 'período' obrigatório" });
+        }
+        const dias = parseInt(periodo.replace("d", ""), 10);
+
+        try {
+            const querry = `
+            SELECT cb.num_conta, SUM(ABS(tr.valor)) AS volume_total
+            FROM conta_bancaria cb
+            JOIN titularidade t ON cb.num_conta = t.fk_num_conta
+            JOIN transacao tr ON cb.num_conta = tr.fk_num_conta
+            WHERE t.fk_cpf_cliente = ? AND tr.data_hora >= DATE_SUB(NOW(), INTERVAL ? DAY)
+            GROUP BY cb.num_conta
+            ORDER BY volume_total DESC
+            `;
+            const [rows] = await pool.query(querry, [cpf, dias]);
+            return reply.status(200).send(rows);
+        } catch (error) {
+            return reply.status(500).send({ error: "Erro ao buscar contas com maior volume de transações" });
+        }
+    };
 }
